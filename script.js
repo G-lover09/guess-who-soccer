720c128fcbe50c87bb1e2098573772a6ab4a846c
G-lover09 <joshua.m.glover09@icloud.com>
2026-02-25 01:02:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function x(element) {
+  // 'element' refers to the button that was clicked
+  element.style.backgroundImage = "url('photos/download.png')";
+  element.style.backgroundSize = "cover";
+  element.style.backgroundRepeat = "no-repeat";
+}
